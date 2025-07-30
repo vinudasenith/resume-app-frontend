2d41b032932297d9c4c070f426de7ed9888aa32a
vinudasenith <ha.vinudas@gmail.com>
2025-07-30 19:12:37 +0530

    create login form UI👍

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+}
