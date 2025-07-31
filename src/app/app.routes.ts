a6881dba6e5705844b7a8ecfef359ce352e283bd
vinudasenith <ha.vinudas@gmail.com>
2025-07-31 19:18:05 +0530

    add feature route👍

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { FeatureComponent } from './pages/feature/feature.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: '', component: HomeComponent },
-    { path: 'contact', component: ContactComponent }
+    { path: 'contact', component: ContactComponent },
+    { path: 'feature', component: FeatureComponent },
 ];
