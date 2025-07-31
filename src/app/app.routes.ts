ddb3fe827c1dc873fa612d4dbd592934206b7cc3
vinudasenith <ha.vinudas@gmail.com>
2025-07-31 12:14:24 +0530

    add register,home page routes👍

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent }
+    { path: 'register', component: RegisterComponent },
+    { path: '', component: HomeComponent }
 ];
