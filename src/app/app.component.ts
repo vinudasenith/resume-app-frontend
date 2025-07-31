df921c9f78b3e694aec0bee16c756ae948a5de55
vinudasenith <ha.vinudas@gmail.com>
2025-07-31 12:13:37 +0530

    render header based on route👍

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,38 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'resume-matcher-frontend';
+  isLoginPage = false;
+  isRegisterPage = false;
+  isHome = false;
+
+
+
+
+
+
+
+
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      const path = event.urlAfterRedirects;
+      this.isLoginPage = path === '/login';
+      this.isRegisterPage = path === '/register';
+      this.isHome = path === '/';
+    });
+  }
 
 }
 
