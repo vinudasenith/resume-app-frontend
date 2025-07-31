0d7eb2a40e294fdea4da858c536ed03eb039534e
vinudasenith <ha.vinudas@gmail.com>
2025-07-31 12:43:50 +0530

    add contact-us page👍

---
+++ src/app/pages/contact/contact.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent {
+
+}
