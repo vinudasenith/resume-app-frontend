4380e6492e2eb2a57bcf7bad5470b93a35abfc84
vinudasenith <ha.vinudas@gmail.com>
2025-07-31 21:46:00 +0530

    add ui for the admin page👍

---
+++ src/app/pages/admin/admin.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './admin.component.html',
+  styleUrl: './admin.component.css'
+})
+export class AdminComponent {
+
+}
