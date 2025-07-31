bad575ad54d26378150246c619ced9ca8447fc12
vinudasenith <ha.vinudas@gmail.com>
2025-07-31 12:44:13 +0530

    add contact page route👍

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: 'contact', component: ContactComponent }
 ];
