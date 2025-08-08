31bea445d81a5f983d137993cdc31a60c517e546
vinudasenith <ha.vinudas@gmail.com>
2025-08-08 09:24:11 +0530

    add router based navigation,process login token, decode user👍

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { environment } from '../../../environments/environment';
+import { AuthService } from '../../services/auth.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, HttpClientModule],
+  imports: [CommonModule, FormsModule, HttpClientModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
@@ -17,7 +19,7 @@ export class LoginComponent {
   password: string = '';
   error: string = '';
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router, private authService: AuthService) { }
 
   handleLogin() {
     const data = {
@@ -28,8 +30,27 @@ export class LoginComponent {
     this.http.post<any>(`${environment.apiBaseUrl}/users/login`, data).subscribe({
       next: (response) => {
         console.log("Login successful", response);
-        alert("✅ Login successful");
-        this.router.navigate(['/']);
+
+        const token = response.token;
+
+
+        console.log("Token:", token);
+
+        if (token) {
+          this.authService.login(token);
+          alert("✅ Login successful");
+
+          const user = this.authService.getCurrentUser();
+          console.log("Decoded User from JWT:", user);
+          if (user?.role === 'admin') {
+            this.router.navigate(['/admin']).catch((err) => console.error('Navigation failed', err));
+          } else {
+            this.router.navigate(['/']).catch((err) => console.error('Navigation failed', err));
+          }
+        } else {
+          alert("Login failed");
+        }
+
       },
       error: (error) => {
         console.log("Login failed", error);
